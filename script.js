9b815cf3e0a0eecf937a7346a3a3cc052e979ff6
xovest <79008917+xovest@users.noreply.github.com>
2021-10-21 16:34:20 +0300

    don't bother with this just need to make some changes cuz im really noob

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function searchShow(query) {
     .then(res => res.json())
     .then(data => {
       const results = data.map(el => el.name);
-      console.log(results); //change with async and await
+      console.log(results); //change with async and await or something, still don't know what's going on with this var or whatever it is but i can't map it, that's for sure.. need to work on that and see how to get json info and also stop overcommenting lmao
     });
 }
 
